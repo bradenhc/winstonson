6e4d0bf9ee4773cce9e940ff6f811212da96071e
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-06-12 16:54:33 -0700

    introducing WinstonsonLogger class, incrementing version

--- src/index.ts
+++ src/index.ts
@@ -8,136 +8,154 @@ let _level: DebugLevel = "debug";
 let _dateFormat: string = null;
 
 const _customLevels = {
-  levels: {
-    error: 0,
-    warn: 1,
-    info: 2,
-    trace: 3,
-    debug: 4
-  },
-  colors: {
-    error: "red",
-    warn: "yellow",
-    info: "cyan",
-    trace: "magenta",
-    debug: "white"
-  }
+    levels: {
+        error: 0,
+        warn: 1,
+        info: 2,
+        trace: 3,
+        debug: 4
+    },
+    colors: {
+        error: "red",
+        warn: "yellow",
+        info: "cyan",
+        trace: "magenta",
+        debug: "white"
+    }
 };
 
 const _logFormatter = winston.format.printf((info) => {
-  const date = _dateFormat ? info.timestamp.format(_dateFormat) : info.timestamp.format();
-  return `${info.level}: ${date} [${info.filename}] ${info.message}`;
+    const date = _dateFormat ? info.timestamp.format(_dateFormat) : info.timestamp.format();
+    return `${info.level}: ${date} [${info.filename}] ${info.message}`;
 });
 
 const _transports = {
-  console: new winston.transports.Console({
-    format: winston.format.combine(winston.format.colorize(), _logFormatter)
-  }),
-  fileGeneral: new winston.transports.File({
-    filename: "out.log",
-    format: _logFormatter
-  }),
-  fileError: new winston.transports.File({
-    filename: "error.log",
-    level: "error",
-    format: _logFormatter
-  })
+    console: new winston.transports.Console({
+        format: winston.format.combine(winston.format.colorize(), _logFormatter)
+    }),
+    fileGeneral: new winston.transports.File({
+        filename: "out.log",
+        format: _logFormatter
+    }),
+    fileError: new winston.transports.File({
+        filename: "error.log",
+        level: "error",
+        format: _logFormatter
+    })
 };
 
 const _logger = winston.createLogger({
-  levels: _customLevels.levels,
-  level: _level,
-  transports: [_transports.fileError, _transports.fileGeneral]
+    levels: _customLevels.levels,
+    level: _level,
+    transports: [_transports.fileError, _transports.fileGeneral]
 });
 
 winston.addColors(_customLevels.colors);
 
 if (process.env.NODE_ENV !== "production") {
-  _logger.add(_transports.console);
+    _logger.add(_transports.console);
 }
 
 if (process.env.NODE_ENV === "test") {
-  _transports.console.silent = true;
+    _transports.console.silent = true;
 }
 
-export default function(m: NodeModule) {
-  const pathParts = m.id.split("/");
-  const filename = pathParts[pathParts.length - 1];
-  return {
-    debug(message: string) {
-      _logger.log({
-        level: "debug",
-        message,
-        timestamp: moment(),
-        filename
-      });
-    },
-    trace(message: string) {
-      _logger.log({ level: "trace", message, timestamp: moment(), filename });
-    },
-    info(message: string) {
-      _logger.log({
-        level: "info",
-        message,
-        timestamp: moment(),
-        filename
-      });
-    },
-    warn(message: string) {
-      _logger.log({
-        level: "warn",
-        message,
-        timestamp: moment(),
-        filename
-      });
-    },
-    error(message: string | Error) {
-      if (typeof message === "object" && message instanceof Error) {
+class WinstonsonLogger {
+    constructor(public filename: string) {}
+
+    public debug(message: string): void {
         _logger.log({
-          level: "error",
-          message: message.message,
-          timestamp: moment(),
-          filename
-        });
-      } else {
-        _logger.log({
-          level: "error",
-          message,
-          timestamp: moment(),
-          filename
-        });
-      }
-    },
-    level(level: DebugLevel) {
-      if (level) {
-        _level = level;
-        _logger.level = level;
-        _transports.console.level = level;
-        _transports.fileGeneral.level = level;
-      } else {
-        return _level;
-      }
-    },
-    mute() {
-      _transports.console.silent = true;
-    },
-    unmute() {
-      _transports.console.silent = false;
-    },
-    setDateFormat(format: string) {
-      _dateFormat = format;
-    },
-    stream(level: DebugLevel) {
-      return {
-        write(message: string, enc: string) {
-          _logger.log({
-            level,
-            message: message.replace(/\n/g, ""), // removing any extra newlines
+            level: "debug",
+            message,
             timestamp: moment(),
-            filename
-          });
-        }
-      };
+            filename: this.filename
+        });
     }
-  };
+
+    public trace(message: string): void {
+        _logger.log({
+            level: "trace",
+            message,
+            timestamp: moment(),
+            filename: this.filename
+        });
+    }
+
+    public info(message: string): void {
+        _logger.log({
+            level: "info",
+            message,
+            timestamp: moment(),
+            filename: this.filename
+        });
+    }
+
+    public warn(message: string) {
+        _logger.log({
+            level: "warn",
+            message,
+            timestamp: moment(),
+            filename: this.filename
+        });
+    }
+
+    public error(message: string | Error) {
+        if (typeof message === "object" && message instanceof Error) {
+            _logger.log({
+                level: "error",
+                message: message.message,
+                timestamp: moment(),
+                filename: this.filename
+            });
+        } else {
+            _logger.log({
+                level: "error",
+                message,
+                timestamp: moment(),
+                filename: this.filename
+            });
+        }
+    }
+
+    public level(level: DebugLevel): string | void {
+        if (level) {
+            _level = level;
+            _logger.level = level;
+            _transports.console.level = level;
+            _transports.fileGeneral.level = level;
+        } else {
+            return _level;
+        }
+    }
+
+    public mute(): void {
+        _transports.console.silent = true;
+    }
+
+    public unmute() {
+        _transports.console.silent = false;
+    }
+
+    public setDateFormat(format: string) {
+        _dateFormat = format;
+    }
+
+    public stream(level: DebugLevel) {
+        return {
+            write(message: string, enc: string) {
+                _logger.log({
+                    level,
+                    message: message.replace(/\n/g, ""), // removing any extra newlines
+                    timestamp: moment(),
+                    filename: this.filename
+                });
+            }
+        };
+    }
+}
+
+export default function(m: NodeModule): WinstonsonLogger {
+    const pathParts = m.id.split("/");
+    const filename = pathParts[pathParts.length - 1];
+    return new WinstonsonLogger(filename);
 }
